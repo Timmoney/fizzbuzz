6d894f627edc887fdc5098be297ae6babd58868f
TimLin <timlin.mail@gmail.com>
2021-09-28 17:24:22 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('index page')
+})
+
+//fizzbuzz api route
+app.get('/api/fizzbuzz/:id', (req, res) => {
+    const result = fizzBuzz(req.params.id);
+    res.send(result);
+})
+
+function fizzBuzz(input) {
+    //check if input is number or not
+    if (!isNumeric(input))
+        return 'Please enter a number...';
+    
+    //check if input is double or integer
+    const num = parseFloat(input);
+    if (!isInteger(num))
+        return 'Please enter a integer...';
+    
+    //other condition
+    if ( num%3 === 0 && num%5 === 0)
+        return 'fizzbuzz';
+    if ( num%3 === 0)
+        return 'fizz';
+    if ( num%5 === 0)
+        return 'buzz';
+
+    return input;
+}
+
+//check if string is a number
+function isNumeric(input) {
+    return !isNaN(input);
+}
+
+//check if number is integer
+function isInteger(input) {
+    return Number.isInteger(input);
+}
+
+app.listen(3000, ()=>{
+    console.log('connect to 3000');
+})
